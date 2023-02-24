3389f9e4176ed828544fcbfb46d5b4502ccff8ea
devRabbani <dev.golamrabbani@gmail.com>
2023-02-25 00:40:10 +0530

    New design

--- src/components/HeroSection/index.jsx
+++ src/components/HeroSection/index.jsx
@@ -2,17 +2,21 @@ export default function HeroSection() {
   return (
     <section className="heroSection">
       <div className="heroSection-body wrapper">
-        <h4>SaITFeedback is only for SaIT College Students</h4>
-        <h1>All in one student's feedback application</h1>
-        <p className="heroSection-para">
-          Our feedback app bridges communication gaps in education. Features
-          include anonymous feedback, OTP verification, admin dashboard,
-          easy-to-read charts. Enhance communication channels and improve
-          education with our feedback app.
-        </p>
-        <button className="btn-getStarted">Get Started for a Demo</button>
+        <div className="heroSection-content">
+          <h4>SaITFeedback is only for SaIT College Students</h4>
+          <h1>All in one student's feedback application</h1>
+          <p className="heroSection-para">
+            Our feedback app bridges communication gaps in education. Features
+            include anonymous feedback, OTP verification, admin dashboard,
+            easy-to-read charts. Enhance communication channels and improve
+            education with our feedback app.
+          </p>
+          <button className="btn-getStarted">Get Started for a Demo</button>
+        </div>
+        <img className="whyus-mockup" src="/src/assets/mockup.png" alt="" />
       </div>
-      <img className="waveBg" src="/src/assets/wave.svg" alt="" />
+
+      {/* <img className="waveBg" src="/src/assets/wave.svg" alt="" /> */}
     </section>
   )
 }
